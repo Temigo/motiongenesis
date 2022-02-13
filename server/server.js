53ad7f896131dcaea19bed4f38174029d5d447f5
Temigo <temigo@gmx.com>
2022-02-12 21:27:00 -0800

    Add file upload

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const Router = require('@koa/router');
 const cors = require('@koa/cors');
 const serve = require('koa-static');
 const path = require('path');
+const koaBody = require('koa-body');
 
 // Load CPP function using named export
 const { String2String, String2Array } = require('./build/Release/hello_world_native.node');
@@ -22,6 +23,15 @@ router.get('/command/:text', (ctx) => {
 });
 router.get('/array/:text', (ctx) => {
     ctx.body = String2Array(ctx.params.text);
+});
+router.post('/upload', koaBody(), (ctx, next) => {
+    // That contains a big string with the text file content
+    // Lines are separated by \n
+    console.log(ctx.request.body.content)
+    let lines = ctx.request.body.content.split('\n');
+    // TODO some more checks...
+    ctx.status = 200;
+    ctx.body = lines.length;
 });
 
 app
--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useRef } from 'react';
 import Terminal, { ColorMode, LineType } from 'react-terminal-ui';
+import './App.css';
 const axios = require('axios');
+
+
 
 // Wraps up the backend call functionality
 function callBackend(terminalInput, route) {
@@ -17,24 +19,78 @@ function callBackend(terminalInput, route) {
         .catch((error) => console.log(error))
 }
 
+function selectFiles(event) {
+    console.log(event)
+}
+
 function App() {
     const [terminalLineData, setTerminalLineData] = useState([
-      {type: LineType.Output, value: 'Welcome to the demo of Motion Genesis in the browser!'}
+      {type: LineType.Output, value: 'Welcome to the demo of Motion Genesis in the browser!'},
+      {type: LineType.Output, value: 'Type `help` to see available commands.'}
     ]);
+
+    const helpString = `
+        Help\n
+        plot\tSend me an array of numbers.\n
+        hello\tGreet me.\n
+        upload\tRead my text file.\n
+        Or type anything else, really.\n
+    `.split('\n');
+    const help = helpString.map((s) => { return { type: LineType.Output, value: s.trim() }; } );
+
+    const filePickerRef = useRef(null);
     return (
         <div className="App">
             <div className="container">
+                <input type="file" style={{"visibility": "hidden", "height": 0, "width": 0, "display": "block"}} ref={filePickerRef} onChange={(e) => {
+                    let reader = new FileReader();
+                    console.log(reader)
+                    // Every time we read a file, send it to the backend via POST request
+                    reader.onloadend = (e) => console.log('loaded', e)
+                    reader.onload = (e) => axios.post(`${ process.env.REACT_APP_BACKEND_URL }/upload/`, {
+                        content: e.target.result
+                    }).then((response) => {
+                        if (response.status == 200) {
+                            setTerminalLineData(prevTerminalLineData => prevTerminalLineData.concat([{
+                                type: LineType.Output, value: 'Uploaded ' + e.target.currentFile.name + '... ' + response.data + ' lines [done]'
+                            }]))
+                        }
+                        else { console.log(response); }
+                    }).catch((error) => console.log(error));
+                    // Now loop over files and read them
+                    const files = [...filePickerRef.current.files];
+                    files.forEach((item, i) => {
+                        console.log(item, i);
+                        // TODO enforce file mime type to be text
+                        reader.currentFile = item;
+                        reader.readAsText(item);
+                    });
+                }} />
                 <Terminal
                     name='Motion Genesis Prototype'
                     colorMode={ ColorMode.Dark }
                     lineData={ terminalLineData }
                     onInput={ (terminalInput) => {
-                        var route = "command";
-                        if (terminalInput == "plot") { route = "array"; }
-                        callBackend(terminalInput, route).then((data) => setTerminalLineData(terminalLineData.concat([
-                            { type: LineType.Input, value: terminalInput },
-                            { type: LineType.Output, value: Array.isArray(data) ? data.join('\n') : data }
-                        ])));
+                        if (terminalInput == "upload") {
+                            filePickerRef.current.click();
+                            setTerminalLineData(prevTerminalLineData => prevTerminalLineData.concat([{ type: LineType.Input, value: terminalInput }]));
+                        }
+                        else if (terminalInput == "help") {
+                            console.log([
+                                { type: LineType.Input, value: terminalInput },
+                                ...help])
+                            setTerminalLineData(prevTerminalLineData => prevTerminalLineData.concat([
+                                { type: LineType.Input, value: terminalInput },
+                                ...help]));
+                        }
+                        else {
+                            var route = "command";
+                            if (terminalInput == "plot") { route = "array"; }
+                            callBackend(terminalInput, route).then((data) => setTerminalLineData(prevTerminalLineData => prevTerminalLineData.concat([
+                                { type: LineType.Input, value: terminalInput },
+                                { type: LineType.Output, value: Array.isArray(data) ? data.join('\n') : data }
+                            ])));
+                        }
                     } }
                 />
             </div>
